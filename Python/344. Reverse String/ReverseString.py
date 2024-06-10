3b587c67381b7ce0840840752c33f195ff000733
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-06-09 23:15:43 -0400

    Update ReverseString.py

--- Python/344. Reverse String/ReverseString.py
+++ Python/344. Reverse String/ReverseString.py
@@ -1,5 +1,12 @@
 class Solution(object):
     def reverseString(self, s):
-        n = len(s)
+        """
+        :type s: List[str]
+        :rtype: None
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)  # Get the length of the input array
+        # Loop from the start of the array to the middle
         for i in range(n // 2):
+            # Swap the character at position i with the character at position n - i - 1
             s[i], s[n - i - 1] = s[n - i - 1], s[i]
