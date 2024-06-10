f213331bb398d4309f6d2f871f3e249a7fe719a1
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-06-09 23:13:29 -0400

    Update ScoreOfAString.cpp

--- C++/3110. Score of a String/ScoreOfAString.cpp
+++ C++/3110. Score of a String/ScoreOfAString.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
     int scoreOfString(string s) {
-        int total = 0;
+        int total = 0;  // Initialize the total score to 0
+        
+        // Loop through each character in the string except the last one
         for(int i = 0; i < s.size() - 1; i++) {
+            // Calculate the absolute difference between adjacent characters
             total += abs(s[i] - s[i + 1]);
         }
+        
+        // Return the accumulated total score
         return total;
     }
 };
