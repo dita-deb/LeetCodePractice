b11f69de5631c72a21d64022f8a6d6a2adf6d34a
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-06-10 23:17:21 -0400

    Update AppendChar2String.cpp

--- C++/2486. Append Characters to String to Make Subsequence/AppendChar2String.cpp
+++ C++/2486. Append Characters to String to Make Subsequence/AppendChar2String.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        int s_index = 0, t_index = 0;
-        int s_length = s.length(), t_length = t.length();
+        int s_index = 0, t_index = 0;  // Initialize indices for both strings
+        int s_length = s.length(), t_length = t.length();  // Get the lengths of both strings
     
+        // Loop through both strings
         while (s_index < s_length && t_index < t_length) {
             if (s[s_index] == t[t_index]) {
-                t_index++;
+                t_index++;  // If characters match, move to the next character in t
             }
-            s_index++;
+            s_index++;  // Always move to the next character in s
         }
     
+        // The number of characters to append is the remaining characters in t
         return t_length - t_index;
     }
 };
